1f09a8098158a8da5cf6a8fdc1465aa104a6e629
bakaneko <baka@nekonyan.net>
2014-11-25 18:01:46 +0900

    implement basic data binding for Swift

--- iosCocoaBindings/Binding.swift
+++ iosCocoaBindings/Binding.swift
@@ -12,8 +12,57 @@ import ObjectiveC
 let bindingContext = UnsafeMutablePointer<()>()
 
 
+/**
+*  Swift-based basic 2-way binding.
+*/
+public class BasicBinding<T: Equatable>: AnyBinding {
+    typealias SourceType = T
+    typealias TargetType = T
 
-public class Binding: NSObject {
+    typealias ObservableType = Observable<T>
+
+
+    var source: ObservableType
+    var target: ObservableType
+
+    var ss: EventSubscription<ValueChange<T>>?
+    var st: EventSubscription<ValueChange<T>>?
+
+    public init(_ source: ObservableType, _ target: ObservableType) {
+        self.source = source
+        self.target = target
+
+        self.ss = self.source.subscribe(.After, observer: sourceObserver)
+        self.st = self.target.subscribe(.After, observer: targetObserver)
+    }
+
+    func sourceObserver(change: ValueChange<T>) {
+        debugPrintln("changing \(change.oldValue) to \(change.newValue), source: \(source.value)")
+        if target.value != change.newValue {
+            target.value = change.newValue
+        }
+    }
+
+    func targetObserver(change: ValueChange<T>) {
+        debugPrintln("changing \(change.oldValue) to \(change.newValue), target: \(target.value)")
+        if source.value != change.newValue {
+            source.value = change.newValue
+        }
+    }
+
+    func unbind() {
+        if let sub = ss {
+            self.source -= sub
+        }
+
+        if let sub = st {
+            self.target -= sub
+        }
+    }
+}
+
+
+public class CocoaBinding: NSObject {
     weak var target: NSObject?
 //    var source: Observable<T>
 
@@ -44,17 +93,17 @@ extension NSObject: NSObjectProtocol {
         return objc_getAssociatedObject(self, &nnicb_bindingKey)
     }
 
-    func nnicb_setBinding(binding: Binding?) {
+    func nnicb_setBinding(binding: CocoaBinding?) {
         objc_setAssociatedObject(self, &nnicb_bindingKey, binding, UInt(bitPattern: OBJC_ASSOCIATION_RETAIN_NONATOMIC))
     }
 
     func bind<T>(key: String, to: Observable<T>) {
-        if let binding = nnicb_binding() as? Binding {
+        if let binding = nnicb_binding() as? CocoaBinding {
             // remove binding
             binding.releaseBindings(self)
         }
 
-        let binding = Binding(source: to, target: self, keyPath: key)
+        let binding = CocoaBinding(source: to, target: self, keyPath: key)
 //        binding.source = to
         nnicb_setBinding(binding)
     }
@@ -64,7 +113,7 @@ extension NSObject: NSObjectProtocol {
     }
 
     func releaseBindings() {
-        if let binding = nnicb_binding() as? Binding {
+        if let binding = nnicb_binding() as? CocoaBinding {
             binding.releaseBindings(self)
         }
     }
