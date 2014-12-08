1b176371f2746c8216a3c4a59b00253d52e480a7
bakaneko <baka@nekonyan.net>
2014-12-08 12:38:15 +0900

    use Foundation instead of UIKit

--- SwiftBindings/SwiftBindings.h
+++ SwiftBindings/SwiftBindings.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 nekonyan. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for SwiftBindings.
 FOUNDATION_EXPORT double SwiftBindingsVersionNumber;
